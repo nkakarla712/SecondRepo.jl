5cef44ce7aff544923c9566fd92afc7078d19c56
Documenter.jl <documenter@juliadocs.github.io>
2023-10-19 17:23:30 +0000

    build based on 9094211

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SecondRepo","category":"page"},{"location":"#SecondRepo","page":"Home","title":"SecondRepo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SecondRepo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SecondRepo]","category":"page"}]
+}
